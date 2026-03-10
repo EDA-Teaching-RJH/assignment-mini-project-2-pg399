35feb294c8dff5e10195f333250d8bd2f247fff9
pg399 <pg399@kent.ac.uk>
2026-03-10 12:45:42 +0000

    Created Contact class for scoring information

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def display_contact(self):
+        return f"Name: {self.name}, Email: {self.email}, Phone: {self.phone}"
+
