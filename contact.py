9197bf6be56126e7b3cf6a9a6156297e43054790
pg399 <pg399@kent.ac.uk>
2026-03-10 23:56:50 +0000

    Added email validation method using regular expression in contact class

--- contact.py
+++ contact.py
@@ -1,8 +1,15 @@
+import re
+
 class Contact:
+
     def __init__(self, name, email, phone):
         self.name = name
         self.email = email
         self.phone = phone
+
+    def validate_email(self):
+        pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        return re.match(pattern, self.email)
 
     def display_contact(self):
         return f"Name: {self.name}, Email: {self.email}, Phone: {self.phone}"
