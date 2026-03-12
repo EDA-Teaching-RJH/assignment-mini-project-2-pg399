74faaddb1b6d0bb4e204f74e50a7a6c5493134a5
pg399 <pg399@kent.ac.uk>
2026-03-12 18:58:23 +0000

    Added comments to explain the function

--- contact.py
+++ contact.py
@@ -1,16 +1,21 @@
-import re
+import re   # Import regex library 
 
+# Contact class store information about a contact 
 class Contact:
 
+
+# Constructor that sets the name, email and phone 
     def __init__(self, name, email, phone):
         self.name = name
         self.email = email
         self.phone = phone
 
+# Function that checks if the email format is valid
     def validate_email(self):
         pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
         return re.match(pattern, self.email)
-
+    
+# Function that returns the contact information    
     def display_contact(self):
         return f"Name: {self.name}, Email: {self.email}, Phone: {self.phone}"
 
