430ed2b745e07f7cb7179d05af82a5213c3185f1
pg399 <pg399@kent.ac.uk>
2026-03-12 13:13:24 +0000

    Added menu system amd functionality to view saved contacts

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 from contact import Contact
-from file_manager import save_contact
+from file_manager import save_contact, load_contacts
 
 def main():
-    print("Contact Manager Application")
+    while True:
+        print("\nContact Manager")
+        print("1. Add Contact")
+        print("2. View Contacts")
+        print("3. Exit")
 
-    name = input("Enter name: ")
-    email = input("Enter email: ")
-    phone = input ("Enter phone number: ")
+        choice = input("Choose an option: ")
 
-    contact = Contact(name, email, phone)
+        if choice =="1":
 
-    if contact.validate_email():
-       save_contact(contact)
-       print("\nContact saved succesfully.")
-       print(contact.display_contact())
-    else:
-        print("Invalid email format. Contact not created. ")
+           name = input("Enter name: ")
+           email = input("Enter email: ")
+           phone = input ("Enter phone number: ")
 
-if __name__ == "__main__":
+           contact = Contact(name, email, phone)
+
+           if contact.validate_email():
+              save_contact(contact)
+              print("Contact saved succesfully.")
+           else:
+          
+              print("Invalid email format.")
+
+        elif choice == "2":
+
+           contacts = load_contacts()
+
+           if contacts:
+              print("\nSaved Contacts:")
+              for c in contacts:
+                  print(f"Name: {c[0]}, Email: {c[1]}, Phone: {c[2]}")
+
+           else:
+                print("No contacts found.")
+
+        
+        elif choice == "3":
+            print("Exiting program. ")
+            break
+        
+        else:
+            print("Invalid option. ")
+
+if __name__ =="__main__":
     main()
+    
+         
