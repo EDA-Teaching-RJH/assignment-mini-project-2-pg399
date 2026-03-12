4d42f5262b06e2af7b94307ebf6d32729c85ed75
pg399 <pg399@kent.ac.uk>
2026-03-12 12:37:21 +0000

    Added function to load contacts from CSV

--- file_manager.py
+++ file_manager.py
@@ -1,6 +1,19 @@
+
 import csv
 
 def save_contact (contact):
     with open("contacts.csv", mode="a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([contact.name, contact.email, contact.phone])
+
+def load_contacts():
+    contacts = []
+
+    try:
+        with open ("contacts.csv", mode="r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                contacts.append(row)
+    except FileNotFoundError:
+        print("No contacts file not found yet. ")
+    return contacts
